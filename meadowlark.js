dd18edd998b90fab8c362bcc30ea91b2afa2a399
renanarantes <malevo18@gmail.com>
2024-03-10 18:46:36 -0300

    refactor to improve testing experience

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
 
-const fortune = require("./libs/fortune");
+const handlers = require("./libs/handlers");
 
 const app = express();
 
@@ -13,24 +13,15 @@ app.use(express.static(__dirname + "/public"));
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", handlers.home);
 
-app.get("/about", (req, res) => {
-  res.render("about", { fortune: fortune.getFortune() });
-});
+app.get("/about", handlers.about);
 
 // pagina 404 personalizada
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
-});
+app.use(handlers.notFound);
 
 // pagina 500 personalizada
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render("500");
-});
+app.use(handlers.serverError);
 
 app.listen(port, () => {
   console.log(`Express started on http://localhost:${port}; press Ctrl-C to terminate.`);
