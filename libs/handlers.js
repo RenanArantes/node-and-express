ee2d716003226517a5296693d66392286e1b3324
renanarantes <malevo18@gmail.com>
2024-03-24 10:20:27 -0300

    cap8 - handling forms pt1

--- libs/handlers.js
+++ libs/handlers.js
@@ -7,3 +7,17 @@ exports.about = (req, res) => res.render("about", { fortune: fortune.getFortune(
 exports.notFound = (req, res) => res.render("404");
 
 exports.serverError = (err, req, res, next) => res.render("500");
+
+exports.newsletterSignup = (req, res) => {
+  res.render("newsletter-signup", { csrf: "CSRF token goes here" });
+};
+
+exports.newsletterSignupProcess = (req, res) => {
+  console.log(`Form (from querystring): ${req.query.form}`);
+  console.log(`CSRF token (from hidden form field): ${req.body._csrf}`);
+  console.log(`Name (from visible form field): ${req.body.name}`);
+  console.log(`Email (from visible form field): ${req.body.email}`);
+  res.redirect(303, "/newsletter-signup/thank-you");
+};
+
+exports.newsletterSignupThankYou = (req, res) => res.render("newsletter-signup-thank-you");
--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 const handlers = require("./libs/handlers");
 const weatherMiddleware = require("./libs/middleware/weather");
@@ -26,6 +27,7 @@ app.set("view engine", "handlebars");
 // app.set("trust proxy", true);
 app.disable("x-powered-by");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 const port = process.env.PORT || 3000;
@@ -33,8 +35,11 @@ const port = process.env.PORT || 3000;
 app.use(weatherMiddleware);
 
 app.get("/", handlers.home);
-
 app.get("/about", handlers.about);
+
+app.get("/newsletter-signup", handlers.newsletterSignup);
+app.post("/newsletter-signup/process", handlers.newsletterSignupProcess);
+app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou);
 
 // pagina 404 personalizada
 app.use(handlers.notFound);
